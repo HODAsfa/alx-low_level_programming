326ec2f5fdfa1e2cf5117d76afdd6d058b80ff9a
HODAsfa <hodasayedfathi@gmail.com>
2023-07-04 15:39:18 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strpbrk - entry point
+ * @s: input
+ * @accept: input
+ * Return: if a set is matched - a pointer to the matched byte.
+ * if no set is matched - NULL
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] !=  '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p - &s[i];
+				return (p);
+			}
+		}
+		j++;
+	}
+	i++;
+
+	return (0);
+}
